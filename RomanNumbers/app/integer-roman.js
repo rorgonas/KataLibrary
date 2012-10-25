bea19641dc576b3a088f1fe784d3bab9414f344f
rorgonas <rorgonas@gmail.com>
2012-10-25 16:23:12 -0400

    Roman Number kata - start working on this

--- RomanNumbers/app/integer-roman.js
+++ RomanNumbers/app/integer-roman.js
@@ -1,6 +1,74 @@
 
-// Create Roman Numbers
-var IntegerToRoman = {
+// Convert convert Roman numbers into Arabic
+
+// Split Roman numbers in units and apply pattern when need
+// At the end unit lenght is the Number
+
+// Symbol	Value
+// I	1
+// V	5
+// X	10
+// L	50
+// C	100
+// D	500
+// M	1,000
 
 
+
+var Roman = {
+
+	toArabic: function(roman){
+		var returnValue;
+		var symbolsValue = {
+			"V": 5,
+			"X": 10
+		}
+
+		var lessValue = {
+			"IV": 4,
+			"IX": 9
+		}
+
+		function addUnit(romanNumber, symbol) {
+			var returnValue;
+			var numberOfI = 0;
+
+			if (roman.match("^" + symbol)) {
+				returnValue = symbolsValue[symbol];
+				numberOfI = roman.split(symbol)[1].length;
+
+				for(var i = 0; i < numberOfI; i++){
+					returnValue += 1;
+				}
+			}
+
+			return returnValue;
+		}
+
+		for (var key in symbolsValue) {
+		  if (symbolsValue.hasOwnProperty(key)) {
+		  	if (roman.match("^" + key)) {
+		  		return addUnit(roman, key);
+		  	}
+		  }
+		}
+
+		// if (roman == "IX"){
+		// 	return 9;
+		// }
+
+		// if (roman == "IV"){
+		// 	return 4;
+		// }
+
+		for (var key in lessValue) {
+		  if (lessValue.hasOwnProperty(key)) {
+		  	if (roman.match("^" + key)) {
+		  		return lessValue[key];
+		  	}
+		  }
+		}
+
+		return roman.length;
+	}
 }
--- RomanNumbers/test/integer-roman.test.js
+++ RomanNumbers/test/integer-roman.test.js
@@ -1,3 +1,41 @@
-// Convert Integer into Roman number
+// Convert Roman number into Arabic
 
 // ---------------------------------------------------------------
+
+test("should return the number of 'I' for I, II, III", function(){
+	equal(Roman.toArabic('I'), 1);
+	equal(Roman.toArabic('II'), 2);
+	equal(Roman.toArabic('III'), 3);
+})
+
+test("IV should return 4", function(){
+	equal(Roman.toArabic('IV'), 4);
+})
+
+test("V should return 5", function(){
+	equal(Roman.toArabic('V'), 5);
+})
+
+test("VI should return 6", function(){
+	equal(Roman.toArabic('VI'), 6);
+})
+
+test("VII should return 7", function(){
+	equal(Roman.toArabic('VII'), 7);
+})
+
+test("VIII should return 8", function(){
+	equal(Roman.toArabic('VIII'), 8);
+})
+
+test("IX should return 9", function(){
+	equal(Roman.toArabic('IX'), 9);
+})
+
+test("X should return 10", function(){
+	equal(Roman.toArabic('X'), 10);
+})
+
+test("XI should return 11", function(){
+	equal(Roman.toArabic('XI'), 11);
+})
