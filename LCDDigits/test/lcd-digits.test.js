0acfce513cc5f30b59c1c9cf5595d18af49e5e9e
rorgonas <rorgonas@gmail.com>
2012-11-29 13:58:44 -0500

    Create LCD Digit kata lib

---
+++ LCDDigits/test/lcd-digits.test.js
@@ -0,0 +1,8 @@
+// TEKCraft - 2012
+// Method: TDD
+// Kata: Creteate LCD Digit represeantation of an integer
+// Author: Rolland Orgonas I.
+// Coach:  Fernando
+
+// ---------------------------------------------------------------
+
