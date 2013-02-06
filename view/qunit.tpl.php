f3543c6dce3b6fbcf89fe7b531df8104d4a9725b
rorgonas <rorgonas@gmail.com>
2013-02-06 14:05:12 -0500

    Rename plugin director

--- view/qunit.tpl.php
+++ view/qunit.tpl.php
@@ -5,8 +5,8 @@
 	<title>QUnit</title>
 	
 	<!-- Include Header -->
-	<link href="http://localhost:8080/KataLibrary/plugin/qunit.css" rel="stylesheet" type="text/css">
-	<script src="http://localhost:8080/KataLibrary/plugin/qunit.js"></script>
+	<link href="http://localhost:8080/KataLibrary/plugins/qunit/qunit.css" rel="stylesheet" type="text/css">
+	<script src="http://localhost:8080/KataLibrary/plugins/qunit/qunit.js"></script>
 
 </head>
 	<body>
@@ -14,7 +14,7 @@
 		
 		<!-- Fizz Buzz - Kata -->
 		<?php 
-
+			// create diff namespace for each qUnit from link alt tab
 			$namespace = 'fizzbuzz';
 			echo '<script src="http://localhost:8080/KataLibrary/dev/app/js/'.$namespace.'.js"></script>';
 			echo '<script src="http://localhost:8080/KataLibrary/dev/test/js/'.$namespace.'.test.js"></script>';
